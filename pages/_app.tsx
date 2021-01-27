9de865f7760de28c8704b258cd1357b8e46af6c7
jvillane <jvillane@gmail.com>
2021-01-27 14:15:59 -0300

    ADD: google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css';
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const isProduction = process.env.NODE_ENV === "production";
+  return (
+    <>
+      <Head>
+        <title>Inmobiliaria Surmonte | conozca la verdadera calidad del servicio a través de la experiencia de sus
+          clientes</title>
+        <meta name="description" content="Servicio de la Inmobiliaria Surmonte al desnudo"/>
+        <meta name="keywords"
+              content="condell54, echeñique46, hl23, candil35, thayer14, ferrer24, lf35, ey32, hda24, wa78, san luis6, capitanes evolución, amudsen evolución, el roble mirador, pocuro miradorpocuro evolución"/>
+        <meta name="author" content="nocumple.cl"/>
+        <meta name="copyright" content="nocumple.cl"/>
+        <meta httpEquiv="cache-control" content="no-cache"/>
+        {isProduction && (
+          <>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-QSX9LNK1C4"/>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag() {dataLayer.push(arguments);}
+              gtag('js', new Date());
+              
+              gtag('config', 'G-QSX9LNK1C4');
+            </script>
+          </>
+        )}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 // Only uncomment this method if you have blocking data requirements for
