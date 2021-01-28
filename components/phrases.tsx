8a157501355de43d6f48390d6b9f23c977999c24
jvillane <jvillane@gmail.com>
2021-01-28 16:10:40 -0300

    FIX: google tag manager

--- components/phrases.tsx
+++ components/phrases.tsx
@@ -5,7 +5,6 @@ interface Props {
 }
 
 export default function Phrases({phrases}: Props) {
-  console.log('phrases', phrases)
   return (
     <>
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,13 +20,13 @@ export default class MyDocument extends Document {
           <meta name="author" content="nocumple.cl"/>
           <meta name="copyright" content="nocumple.cl"/>
           <meta httpEquiv="cache-control" content="no-cache"/>
-          <script
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-QSX9LNK1C4"/>
+          <script async
             dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${GTM_ID}');`,
+              __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '${GTM_ID}');`,
             }}
           />
           <noscript
