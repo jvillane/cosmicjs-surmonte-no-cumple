9bf2cb3f137a5657af88cb716ff1644f6cfd8cec
jvillane <jvillane@gmail.com>
2021-02-04 10:09:58 -0300

    ADD: Google Analytics Tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,7 @@ export default class MyDocument extends Document {
   }
   
   render() {
+    const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
     const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
     
     return (
@@ -21,18 +22,19 @@ export default class MyDocument extends Document {
           <meta name="author" content="nocumple.cl"/>
           <meta name="copyright" content="nocumple.cl"/>
           <meta httpEquiv="cache-control" content="no-cache"/>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-QSX9LNK1C4"/>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}/>
           <script async
             dangerouslySetInnerHTML={{
               __html: `window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
+  gtag('config', '${GA_ID}');
   gtag('config', '${GTM_ID}');`,
             }}
           />
           <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"/>`,
             }}
           />
         </Head>
