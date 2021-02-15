64a07b32b5cebd524ff5f7326247556f5363912d
jvillane <jvillane@gmail.com>
2021-02-14 23:03:35 -0300

    DONE: primary secondary pallete colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: 'var(--color-primary)',
+      secondary: 'var(--color-secondary)',
+    },
     extend: {},
   },
   variants: {
